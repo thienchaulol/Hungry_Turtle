9311a7d178915c074a671938133f759858491336
thienchaulol <tchau006@ucr.edu>
2017-06-21 19:55:46 -0700

    New script, Edited Game Scenes
    New script: - Added game_manager script. Script will control general
    game functions (switching scenes, pause, rewards, end game, begin, etc.)
    
    Edited Game Scenes: User can now switch to rewards scene and back to
    menu scene. User can also switch to in_game scene but cannot exit. In
    game scene will exit when user loses.

---
+++ Assets/game_manager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class game_manager : MonoBehaviour {
+
+    bool paused = false;
+
+	void Update () {
+		
+	}
+
+    public void play_button()
+    {
+        SceneManager.LoadScene("In_Game", LoadSceneMode.Single);
+    }
+
+    public void reward_button()
+    {
+        SceneManager.LoadScene("Rewards", LoadSceneMode.Single);
+    }
+
+    public void menu_button()
+    {
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
+
+    public void pause_button()
+    {
+        //If paused is true, pause the game.
+        paused = !paused;
+        Debug.Log(paused);
+    }
+}
