d0463d9db51bb5b7681a61d5a675c96c7584147d
thienchaulol <tchau006@ucr.edu>
2017-06-22 16:48:42 -0700

    In Game Changes
    New Script: sprite_control.cs will control the food sprite that the
    turtles will eat.
    
    Food Sprite: Food sprite will appear in a puff of smoke and drop to the
    top of the water. Then the food will float for 3 seconds and then sink.
    After sinking for X seconds, the food will expire and dirty the tank.

---
+++ Assets/In_Game_Scripts/object_pooler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class object_pooler : MonoBehaviour {
+
+    public GameObject pooledObject; //gameobject used to pool objects
+    public int pooledAmount;    //initial number of pooled objects
+    List<GameObject> pooledObjects; //list of pooled objects
+
+    void Start()
+    {
+        pooledObjects = new List<GameObject>(); //allocate new list of game objects
+
+        for (int i = 0; i < pooledAmount; i++)
+        {   //create initial number of pooled objects
+            GameObject obj = (GameObject)Instantiate(pooledObject); //instantiate(create) object
+            obj.SetActive(false);   //set objects to false until they are needed
+            pooledObjects.Add(obj); //add onto list
+        }
+    }
+
+    public GameObject GetPooledObject()
+    {   //called in other functions to acquire UNACTIVE pooled object
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {   //iterate through list of objects
+            if (!pooledObjects[i].activeInHierarchy)
+            {   //if the object is not being used
+                return pooledObjects[i];                //return that object
+            }
+        }
+
+        //if all objects in the list are being used, create a new object
+        GameObject obj = (GameObject)Instantiate(pooledObject); //create new object
+        obj.SetActive(false);   //set it to false
+        pooledObjects.Add(obj); //add it to the list for future use
+        return obj; //return this new object to be used
+    }
+}
---
+++ Assets/In_Game_Scripts/sprite_control.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sprite_control : MonoBehaviour {
+
+    public float fallSpeed;
+    public float sinkSpeed;
+    public float expirationTime;
+    bool sinking = false;
+    bool expired = false;
+    public Transform floatingPoint;
+
+    void OnEnable()
+    {
+        //The food will appear in a puff of smoke above the water and fall
+    }
+
+    void Update()
+    {
+        if (gameObject.transform.position.y > floatingPoint.position.y)
+        {
+            Movement();
+        }
+        if (gameObject.transform.position.y <= floatingPoint.position.y && !sinking)
+        {
+            //Give the sprite a "floating" animation for 3 seconds
+            //After 3 seconds, the sprite will beging to sink
+            StartCoroutine(waitThreeSeconds());
+        }
+        if (sinking && !expired)
+        {
+            transform.Translate(Vector2.down * sinkSpeed * Time.deltaTime);
+            //After X amount of time, have the food expire and make the tank dirty
+            StartCoroutine(waitXSeconds(expirationTime));
+        }
+    }
+
+    void Movement()
+    {
+        transform.Translate(Vector2.down * fallSpeed * Time.deltaTime);
+    }
+
+    IEnumerator waitThreeSeconds()
+    {
+        yield return new WaitForSeconds(3);
+        sinking = true;
+    }
+
+    IEnumerator waitXSeconds(float X)
+    {
+        yield return new WaitForSeconds(X);
+        expired = true;
+        sinking = false;
+    }
+}
